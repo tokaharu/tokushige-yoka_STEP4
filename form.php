d300b326afbca9f54c6c77ed708bae9c69884abc
tokaharu <yoka27.tokappe01020@gmail.com>
2026-08-29 10:34:10 +0900

    入力フォーム作成

--- form.php
+++ form.php
@@ -4,35 +4,36 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>フォーム</title>
-  <link rel="stylesheet" href="style.php">
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <h1>フォーム入力</h1>
   <form action="confirm.php" method="post">
     <label for="name">名前：</label>
-    <input type="text" id="name" name="name"><br><br>
+    <input type="text" id="name" name="name" placeholder="例）田中　太郎"><br><br>
 
     <label for="age">年齢：</label>
-    <input type="number" id="age" name="age"><br><br>
+    <input type="number" id="age" name="age" placeholder="例）20"><br><br>
 
-    <label for="tell">電話番号：</label>
-    <input type="number" id="tell" name="tell"><br><br>
+    <label for="phone">電話番号：</label>
+    <input type="text" id="phone" name="phone" placeholder="例）000-0000-0000 or 00000000000"><br><br>
 
-    <label for="mail">メールアドレス：</label>
-    <input type="email" id="mail" name="mail"><br><br>
+    <label for="email">メールアドレス：</label>
+    <input type="email" id="email" name="email" placeholder="例）〇〇@gmail.com"><br><br>
 
     <label for="address">住所：</label>
-    <input type="text" id="address" name="address"><br><br>
+    <input type="text" id="address" name="address" placeholder="都道府県から入力してください。"><br><br>
 
     <label for="question">質問：</label>
-    <input type="text" id="question" name="question"><br><br>
+    <input type="text" id="question" name="question" placeholder="ご質問を入力してください。"><br><br>
 
     <label for="gender">性別：</label>
     <select name="gender" id="gender">
-      <option value="male">男性
-      </option>
-      <option value="female">女性</option>
-    </select>
+      <option value="選択してください">選択してください</option>
+      <option value="男性">男性</option>
+      <option value="女性">女性</option>
+      <option value="その他">その他</option>
+    </select><br><br>
 
     <button type="submit">送信</button>
 
